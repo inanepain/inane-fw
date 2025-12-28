ec4aa15a40b2cbf1d44f6b7dcd580f572dbb3d1b
Philip Michael Raab <philip@cathedral.co.za>
2025-12-29 01:19:45 +0200

    Initialize project debug configurations, app settings, and database query templates.

---
+++ config/app.config.php
@@ -0,0 +1,58 @@
+<?php
+
+/**
+ * Skeleton: Inane-FW
+ *
+ * Web or console application framework using the inanepain libraries.
+ *
+ * $Id$
+ * $Date$
+ *
+ * PHP version 8.4
+ *
+ * @author Philip Michael Raab<philip@cathedral.co.za>
+ * @package skeleton\inane-fw
+ * @category inane-fw
+ *
+ * @license UNLICENSE
+ * @license https://unlicense.org/UNLICENSE UNLICENSE
+ *
+ * _version_ $version
+ */
+
+declare(strict_types=1);
+
+return [
+	'appId' => 'inane-fw',
+	'config' => [
+		'glob_pattern' => realpath(__DIR__) . '/autoload/{{,*.}global,{,*.}local}.php',
+		'allow_modifications' => false,
+	],
+	'view'   => [
+		'path'   => 'View',
+		// 'layout' => 'layout/layout',
+		'layout' => 'layout/lo2',
+	],
+	'router' => [
+		'splitQuerystring' => true,
+		'controller' => [
+			'glob' => 'src/*/*Controller.php',
+			'glob_ignore' => '/(Abstract)/',
+			'default' => [
+				\Dev\Controller\Test::class,
+			],
+		],
+	],
+	/**
+	 * Default services
+	 */
+	'services' => [],
+	/**
+	 * Server
+	 */
+	'server' => [
+		'host' => '0.0.0.0',
+		'port' => 9502,
+	],
+	'components' => [],
+];
