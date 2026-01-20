d21782f3db3d534bd1b5cff8e0007aa41cb5a194
Philip Michael Raab <philip@cathedral.co.za>
2026-01-20 21:07:44 +0200

    Documentation add more code comments

--- src/Brew/Brew.php
+++ src/Brew/Brew.php
@@ -1,6 +1,6 @@
 <?php
-/*
- *
+
+/**
  * Inane: PROJECT
  *
  * PROJECT_DESCRIPTION
@@ -8,7 +8,7 @@
  * $Id$
  * $Date$
  *
- * PHP version 8.4
+ * PHP version 8.5
  *
  * @author   Philip Michael Raab <philip@cathedral.co.za>
  * @package  inanepain\PROJECT
@@ -21,8 +21,11 @@
  *
  */
 
+declare(strict_types=1);
+
 namespace Knot\Brew;
 
+use Inane\Stdlib\Exception\RuntimeException;
 use Inane\Stdlib\Options;
 use Knot\Db\Entity\Formula;
 use Knot\Db\Table\FormulasTable;
@@ -35,15 +38,28 @@ class Brew {
     //#region Properties
     private array $tags;
     protected Options $cache;
-
     //#endregion Properties
 
+    /**
+     * Constructor for the Brew class.
+     *
+     * Initializes the object with a FormulasTable instance and sets up a cache.
+     *
+     * @param FormulasTable $formulasTable An instance of the FormulasTable class to interact with the database.
+     */
     public function __construct(
         private FormulasTable $formulasTable = new FormulasTable(),
     ) {
         $this->cache = new Options();
     }
 
+    /**
+     * Retrieves formulas that have tags associated with them.
+     *
+     * @return Options Returns an Options object containing the tagged formulas.
+     *
+     * @throws RuntimeException
+     */
     public function getTagged(): Options {
         $key = 'tagged';
         if (!$this->cache->has($key)) {
@@ -54,6 +70,13 @@ class Brew {
         return $this->cache->get($key);
     }
 
+    /**
+     * Retrieves formulas that are pending review.
+     *
+     * @return Options Returns an Options object containing the reviewed formulas.
+     *
+     * @throws RuntimeException
+     */
     public function getReview(): Options {
         $key = 'review';
         if (!$this->cache->has($key)) {
@@ -64,6 +87,13 @@ class Brew {
         return $this->cache->get($key);
     }
 
+    /**
+     * Generates a list of tags from the formulas table.
+     *
+     * @return array Returns an associative array with tag names as keys and their counts as values.
+     *
+     * @throws RuntimeException
+     */
     public function getTags(): array {
         if (!isset($this->tags)) {
             $formulas = $this->getTagged();
@@ -78,8 +108,6 @@ class Brew {
                 }
             }
             ksort($tags);
-            //            $tags = array_unique(array_filter(explode(',', array_reduce($formulas->toArray(), fn($carry, $formula) => $carry . ',' . $formula->tags, ''))));
-            //            sort($tags);
 
             $this->tags = $tags;
         }
@@ -88,22 +116,22 @@ class Brew {
     }
 
     /**
-     * Retrieves the specified package from the formulas table.
+     * Retrieves a specific package by name.
      *
      * @param string $package The name of the package to retrieve.
      *
-     * @return Formula|false The Formula object if found, or false if not found.
+     * @return Formula|false Returns the Formula object if found, or false if not found.
      */
     public function getPackage(string $package): Formula|false {
         return $this->formulasTable->fetch($package);
     }
 
     /**
-     * Retrieves a list of packages from the formulas table based on the provided package names.
+     * Retrieves multiple packages by their names.
      *
-     * @param string ...$package The names of the packages to retrieve.
+     * @param string ...$package Variable number of package names to retrieve.
      *
-     * @return Formula[] An array of matching packages retrieved from the formulas table.
+     * @return Formula[] Returns an array of Formula objects matching the provided package names.
      */
     public function getPackages(string ...$package): array {
         return $this->formulasTable->find([['type' => 'in', 'column' => 'name', 'values' => $package]]);
