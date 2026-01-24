9ed4ab09901bacaef5f2704f6a803c255bf049b2
Philip Michael Raab <philip@cathedral.co.za>
2026-01-24 04:44:51 +0200

    Refactor application initialization and enhance config handling
    Replaced manual application initialization with automatic instance creation using `Config::fromConfigFile()`. Simplified `ServiceManager` setup, improved error handling, and adjusted `Dumper` configuration for better compatibility. Added new views and updated database schema for user and department management. Updated `.gitignore` to exclude `/tmp/`.

--- DB/main/users.sql
+++ DB/main/users.sql
@@ -1,18 +1,18 @@
 CREATE TABLE users (
 	id           INTEGER
 		PRIMARY KEY AUTOINCREMENT,
-	iddepartment INTEGER  DEFAULT 1     NOT NULL
+	iddepartment INTEGER  DEFAULT 1          NOT NULL
 		CONSTRAINT fk_departments
 			REFERENCES departments,
-	username     TEXT(15)               NOT NULL
+	username     TEXT(15)                    NOT NULL
 		CONSTRAINT uniq_usernmae
 			UNIQUE,
-	password TEXT(50) DEFAULT "chAng3M!" NOT NULL,
-	online       INT      DEFAULT 0     NOT NULL,
-	name         TEXT(20)               NOT NULL,
-	email        TEXT(20)               NOT NULL,
-	"group"      TEXT(10) DEFAULT users NOT NULL,
-	rank         INTEGER  DEFAULT 5     NOT NULL);
+	online       INT      DEFAULT 0          NOT NULL,
+	name         TEXT(20)                    NOT NULL,
+	email        TEXT(20)                    NOT NULL,
+	"group"      TEXT(10) DEFAULT users      NOT NULL,
+	rank         INTEGER  DEFAULT 5          NOT NULL,
+	password     TEXT(50) DEFAULT "chAng3M!" NOT NULL);
 
 CREATE INDEX idx_online
 	ON users (online);
