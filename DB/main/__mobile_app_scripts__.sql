4e5f97481449d68e52ee22dd9b3b0d0166e87906
Philip Michael Raab <philip@cathedral.co.za>
2026-02-06 22:18:52 +0200

    Moved database source files and updated documentation.
    * Moved all database source files from `DB` to `source/database`.
    * Updated structure documentation.
    * Improving `README` with a new **Develop** section and instructions for submodule setup.
    * Updated submodules to track the `develop` branch.

--- DB/main/__mobile_app_scripts__.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE __mobile_app_scripts__ (
-	ID                     TEXT
-		PRIMARY KEY,
-	SCRIPT                 TEXT,
-	LAST_CHANGE            REAL,
-	PIN                    BOOLEAN,
-	CONDITIONAL_FORMATTING TEXT,
-	NAME                   TEXT);
-
