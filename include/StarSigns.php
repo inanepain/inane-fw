58b32efa3470285b4c0f11803c25e4582dcb4259
Philip Michael Raab <philip@cathedral.co.za>
2026-06-01 21:34:14 +0200

    Inane-FW - project phpstorm config - StarSigns

---
+++ include/StarSigns.php
@@ -0,0 +1,190 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Generated
+ * - by: ChatGPT
+ * - model: GPT-5.5
+ *
+ * duration: instant
+ */
+$starSigns = [
+    'aries' => [
+        'starSign' => 'aries',
+        'symbol'   => '♈',
+        'start'    => 'Mar 21',
+        'end'      => 'Apr 19',
+    ],
+    'taurus' => [
+        'starSign' => 'taurus',
+        'symbol'   => '♉',
+        'start'    => 'Apr 20',
+        'end'      => 'May 20',
+    ],
+    'gemini' => [
+        'starSign' => 'gemini',
+        'symbol'   => '♊',
+        'start'    => 'May 21',
+        'end'      => 'Jun 20',
+    ],
+    'cancer' => [
+        'starSign' => 'cancer',
+        'symbol'   => '♋',
+        'start'    => 'Jun 21',
+        'end'      => 'Jul 22',
+    ],
+    'leo' => [
+        'starSign' => 'leo',
+        'symbol'   => '♌',
+        'start'    => 'Jul 23',
+        'end'      => 'Aug 22',
+    ],
+    'virgo' => [
+        'starSign' => 'virgo',
+        'symbol'   => '♍',
+        'start'    => 'Aug 23',
+        'end'      => 'Sep 22',
+    ],
+    'libra' => [
+        'starSign' => 'libra',
+        'symbol'   => '♎',
+        'start'    => 'Sep 23',
+        'end'      => 'Oct 22',
+    ],
+    'scorpio' => [
+        'starSign' => 'scorpio',
+        'symbol'   => '♏',
+        'start'    => 'Oct 23',
+        'end'      => 'Nov 21',
+    ],
+    'sagittarius' => [
+        'starSign' => 'sagittarius',
+        'symbol'   => '♐',
+        'start'    => 'Nov 22',
+        'end'      => 'Dec 21',
+    ],
+    'capricorn' => [
+        'starSign' => 'capricorn',
+        'symbol'   => '♑',
+        'start'    => 'Dec 22',
+        'end'      => 'Jan 19',
+    ],
+    'aquarius' => [
+        'starSign' => 'aquarius',
+        'symbol'   => '♒',
+        'start'    => 'Jan 20',
+        'end'      => 'Feb 18',
+    ],
+    'pisces' => [
+        'starSign' => 'pisces',
+        'symbol'   => '♓',
+        'start'    => 'Feb 19',
+        'end'      => 'Mar 20',
+    ],
+];
+
+
+/**
+ * Generated
+ * - by: Ollama
+ * - model: gwen3.6
+ *
+ * duration: ages
+ */
+$zodiacSigns = [
+    'aries' => [
+        'starSign' => 'aries',
+        'symbol'   => '♈',
+        'start'    => 'Mar 21',
+        'end'      => 'Apr 19',
+    ],
+    'taurus' => [
+        'starSign' => 'taurus',
+        'symbol'   => '♉',
+        'start'    => 'Apr 20',
+        'end'      => 'May 20',
+    ],
+    'gemini' => [
+        'starSign' => 'gemini',
+        'symbol'   => '♊',
+        'start'    => 'May 21',
+        'end'      => 'Jun 20',
+    ],
+    'cancer' => [
+        'starSign' => 'cancer',
+        'symbol'   => '♋',
+        'start'    => 'Jun 21',
+        'end'      => 'Jul 22',
+    ],
+    'leo' => [
+        'starSign' => 'leo',
+        'symbol'   => '♌',
+        'start'    => 'Jul 23',
+        'end'      => 'Aug 22',
+    ],
+    'virgo' => [
+        'starSign' => 'virgo',
+        'symbol'   => '♍',
+        'start'    => 'Aug 23',
+        'end'      => 'Sep 22',
+    ],
+    'libra' => [
+        'starSign' => 'libra',
+        'symbol'   => '♎',
+        'start'    => 'Sep 23',
+        'end'      => 'Oct 22',
+    ],
+    'scorpio' => [
+        'starSign' => 'scorpio',
+        'symbol'   => '♏',
+        'start'    => 'Oct 23',
+        'end'      => 'Nov 21',
+    ],
+    'sagittarius' => [
+        'starSign' => 'sagittarius',
+        'symbol'   => '♐',
+        'start'    => 'Nov 22',
+        'end'      => 'Dec 21',
+    ],
+    'capricorn' => [
+        'starSign' => 'capricorn',
+        'symbol'   => '♑',
+        'start'    => 'Dec 22',
+        'end'      => 'Jan 19',
+    ],
+    'aquarius' => [
+        'starSign' => 'aquarius',
+        'symbol'   => '♒',
+        'start'    => 'Jan 20',
+        'end'      => 'Feb 18',
+    ],
+    'pisces' => [
+        'starSign' => 'pisces',
+        'symbol'   => '♓',
+        'start'    => 'Feb 19',
+        'end'      => 'Mar 20',
+    ],
+];
+
+/**
+ * Generated
+ * - by: Ollama
+ * - model: phi4-mini
+ *
+ * duration: quick
+ */
+$stars = [
+    'aries' => ['starSign' => 'aries', 'symbol' => '♈', 'start' => 'Mar 21', 'end' => 'Apr 19'],
+    'taurus' => ['starSign' => 'taurus', 'symbol' => '♉', 'start' => 'Apr 20', 'end' => 'May 20'],
+    'gemini' => ['starSign' => 'gemini', 'symbol' => '♊', 'start' => 'May 21', 'end' => 'Jun 20'],
+    'cancer' => ['starSign' => 'cancer', 'symbol' => '♋', 'start' => 'Jun 21', 'end' => 'Jul 22'],
+    'leo' => ['starSign' => 'leo', 'symbol' => '♌', 'start' => 'Jul 23', 'end' => 'Aug 22'],
+    'virgo' => ['starSign' => 'virgo', 'symbol' => '♍', 'start' => 'Aug 23', 'end' => 'Sep 22'],
+    'libra' => ['starSign' => 'libra', 'symbol' => '♎', 'start' => 'Sep 23', 'end' => 'Oct 22'],
+    'scorpio' => ['starSign' => 'scorpio', 'symbol' => '♏', 'start' => 'Oct 23', 'end' => 'Nov 21'],
+    'sagittarius' => ['starSign' => 'sagittarius', 'symbol' => '♐', 'start' => 'Nov 22', 'end' => 'Dec 21'],
+    'capricorn' => ['starSign' => 'capricorn', 'symbol' => '♑', 'start' => 'Dec 22', 'end' => 'Jan 19'],
+    'aquarius' => ['starSign' => 'aquarius', 'symbol' => '♒', 'start' => 'Jan 20', 'end' => 'Feb 18'],
+    'pisces' => ['starSign' => 'pisces', 'symbol' => '♓', 'start' => 'Feb 19', 'end' => 'Mar 20']
+];
